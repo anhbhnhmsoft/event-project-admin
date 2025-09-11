1712308bf0fe0228f68e5735e2d26d8fb3f5134f
maiduong <duongmdph40323@gmail.com>
2025-09-11 14:32:18 +0700

    Duongmd - 6 - Manager Organizer

---
+++ app/Filament/Resources/Organizers/OrganizerResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Resources\Organizers;
+
+use App\Filament\Resources\Organizers\Pages\CreateOrganizer;
+use App\Filament\Resources\Organizers\Pages\EditOrganizer;
+use App\Filament\Resources\Organizers\Pages\ListOrganizers;
+use App\Models\Organizer;
+use App\Filament\Resources\Organizers\Schemas\OrganizerSchema;
+use App\Filament\Resources\Organizers\Tables\OrganizerTable;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+
+class OrganizerResource extends Resource
+{
+    protected static ?string $model = Organizer::class;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBuildingOffice;
+    protected static ?string $navigationLabel = 'Nhà tổ chức';
+    protected static ?string $modelLabel = 'Nhà tổ chức';
+    protected static ?string $pluralModelLabel = 'Nhà tổ chức';
+
+    public static function form(Schema $schema): Schema
+    {
+        return OrganizerSchema::configure($schema);
+    }
+
+    public static function table(\Filament\Tables\Table $table): \Filament\Tables\Table
+    {
+        return OrganizerTable::configure($table);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListOrganizers::route('/'),
+            'create' => CreateOrganizer::route('/create'),
+            'edit' => EditOrganizer::route('/{record}/edit'),
+        ];
+    }
+}
+
+
