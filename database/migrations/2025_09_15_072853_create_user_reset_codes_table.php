0e3bde06f3631ee3cf2ad981f96a69b08c7b6b67
maiduong <duongmdph40323@gmail.com>
2025-09-15 14:01:32 +0700

    Create Table User Reset Codes

---
+++ database/migrations/2025_09_15_072853_create_user_reset_codes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_reset_codes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('email');
+            $table->string('code', 6);
+            $table->timestamp('expires_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_reset_codes');
+    }
+};
