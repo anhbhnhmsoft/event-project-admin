a47f84ea49f26b9af7a3a20310af4abe196c2d95
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-09-19 10:25:28 +0700

    AnhBH - create table membership user

---
+++ database/migrations/2025_09_19_102331_create_membership_user.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('membership_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->foreignId('membership_id')
+                ->constrained('membership')
+                ->cascadeOnDelete();
+            $table->date('start_date')->nullable()
+                ->comment('Ngày bắt đầu gói membership');
+            $table->date('end_date')->nullable()
+                ->comment('Ngày kết thúc gói membership');
+            $table->tinyInteger('status')->comment('Trạng thái gói: enum định nghĩa MembershipUserStatus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('membership_user');
+    }
+};
