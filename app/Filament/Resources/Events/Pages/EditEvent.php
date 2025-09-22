b2b86a690b6e3ca9b2050f1745df0a1c97dc8b5d
anhbhnhmsoft <anhbhnhmsoft@gmail.com>
2025-09-22 10:38:50 +0700

    AnhBH - fix

--- app/Filament/Resources/Events/Pages/EditEvent.php
+++ app/Filament/Resources/Events/Pages/EditEvent.php
@@ -71,7 +71,7 @@ class EditEvent extends EditRecord
             ];
         })->toArray();
         $data['schedules'] = $schedules;
-        
+
         $participants = $event->participants()->get()->map(function ($participant) {
             return [
                 'id' => $participant->id,
@@ -146,7 +146,7 @@ class EditEvent extends EditRecord
 
             if (isset($data['participants'])) {
                 $processedParticipantIds = [];
-                
+
                 foreach (array_values($data['participants']) as $participant) {
                     if (!empty($participant['user_id']) && !empty($participant['role'])) {
                         $eventUser = EventUser::updateOrCreate(
@@ -159,11 +159,11 @@ class EditEvent extends EditRecord
                                 'role' => $participant['role'],
                             ]
                         );
-                        
+
                         $processedParticipantIds[] = $eventUser->id;
                     }
                 }
-                
+
                 EventUser::where('event_id', $record->id)
                     ->whereNotIn('id', $processedParticipantIds)
                     ->delete();
@@ -216,29 +216,25 @@ class EditEvent extends EditRecord
 
                                     $processedDocumentIds[] = $eventScheduleDocument->id;
 
-                                        if (!empty($documentData['files'])) {
-                                            $files = is_array($documentData['files']) ? $documentData['files'] : [$documentData['files']];
-                                            $existingFilePaths = [];
+                                    if (!empty($documentData['files'])) {
+                                        $files = is_array($documentData['files']) ? $documentData['files'] : [$documentData['files']];
+                                        $existingFilePaths = [];
 
-                                            foreach ($files as $file) {
-                                                $tempFile = $this->extractTemporaryFile($file);
+                                        foreach ($files as $file) {
+                                            $tempFile = $this->extractTemporaryFile($file);
 
-                                                if ($tempFile) {
-                                                    $filePath = $tempFile->store(
-                                                        StoragePath::makePathById(StoragePath::EVENT_PATH, $record->id) . '/' . $eventSchedule->id . '/' . $eventScheduleDocument->id,
-                                                        'public'
-                                                    );
+                                            if ($tempFile) {
+                                                $filePath = $tempFile->store(
+                                                    StoragePath::makePathById(StoragePath::EVENT_PATH, $record->id) . '/' . $eventSchedule->id . '/' . $eventScheduleDocument->id,
+                                                    'public'
+                                                );
 
-                                                    $this->createFileRecord($eventScheduleDocument->id, $tempFile, $filePath);
+                                                $this->createFileRecord($eventScheduleDocument->id, $tempFile, $filePath);
 
-                                                    $existingFilePaths[] = $filePath;
-                                                } elseif (is_string($file)) {
-                                                    $existingFilePaths[] = $file;
-                                                }
+                                                $existingFilePaths[] = $filePath;
+                                            } elseif (is_string($file)) {
+                                                $existingFilePaths[] = $file;
                                             }
-
-                                            $filesToDelete = $this->collectFilesToDelete($eventScheduleDocument->id, $existingFilePaths, $files);
-                                            $allFilesToDelete = array_merge($allFilesToDelete, $filesToDelete);
                                         }
 
                                         $filesToDelete = $this->collectFilesToDelete($eventScheduleDocument->id, $existingFilePaths, $files);
@@ -386,7 +382,7 @@ class EditEvent extends EditRecord
         $filePaths = array_column($filesToDelete, 'file_path');
         $fileIds = array_column($filesToDelete, 'id');
 
-        $existingPaths = array_filter($filePaths, function($path) {
+        $existingPaths = array_filter($filePaths, function ($path) {
             return Storage::disk('public')->exists($path);
         });
 
