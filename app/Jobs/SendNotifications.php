101ff67de22fd4b49ec61874108207338d5d1298
Truonghocdot <truonghocdot05@gmail.com>
2025-10-10 18:38:03 +0700

    Truonghd - Fix

--- app/Jobs/SendNotifications.php
+++ app/Jobs/SendNotifications.php
@@ -72,7 +72,7 @@ class SendNotifications implements ShouldQueue
             });
             if (empty($batch)) {
                 DB::rollBack();
-                Log::default('SendNotifications: Không có batch để gửi');
+                Log::debug('SendNotifications: Không có batch để gửi');
                 return;
             }
             DB::commit();
