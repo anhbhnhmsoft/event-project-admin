3aa495757b702b62681882d02349b00638437b92
Truonghocdot <truonghocdot05@gmail.com>
2025-10-20 15:44:01 +0700

    Truonghd - Fix

---
+++ database/migrations/2025_10_20_154039_add_organizer_id_to_transaction_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->foreignId('organizer_id')
+                ->constrained('organizers')
+                ->onDelete('cascade')
+                ->comment('Khóa ngoại tới bảng organizer')
+                ->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['organizer_id']);
+            $table->dropColumn('organizer_id');
+        });
+    }
+};
